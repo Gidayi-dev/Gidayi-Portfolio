eb91dbf3ea3b3ceb7462edfd07a77632a39cfbd5
Gidayi Millyannah <millyannahi@gmail.com>
2024-10-21 11:21:14 +0300

    hero-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+import Hero from "./components/hero";
 import './App.css';
 import Sidebar from "./components/sidebar";
-import Hero from "./components/hero";
 import AboutMe from './components/about';
 import Projects from './components/projects';
 import Skills from './components/skills';
